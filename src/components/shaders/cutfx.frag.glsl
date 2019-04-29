b50300be6168dee6ce1fc64fb2668d331617ac6c
Diego F. Goberna <diego@feiss.be>
2019-04-29 13:16:54 +0200

    removed cut part from fx (fixes glitches)

--- src/components/shaders/cutfx.frag.glsl
+++ src/components/shaders/cutfx.frag.glsl
@@ -1,5 +1,4 @@
 varying vec2 uvs;
-varying float cutProgress;
 uniform sampler2D src;
 uniform vec3 color;
 uniform float progress;
@@ -8,12 +7,10 @@ void main() {
   vec4 tex = texture2D(src, uvs);
   float isGlow = step(0.5, uvs.y);
   float isArc =  step(0.05, uvs.y) - step(0.51, uvs.y);
-  float isCut =  1.0 - step(0.05, uvs.y);
 
   vec3 col = mix(tex.rgb, color, isGlow);
 
   float alpha = smoothstep(progress, 1.0, tex.a);
   alpha = mix(alpha, smoothstep(1.0 - sin(progress * 3.1415), 1.0, tex.a), isArc);
-  alpha = mix(alpha, smoothstep(cutProgress, 1.0, tex.a), isCut);
   gl_FragColor = vec4(col, alpha);
 }
--- src/components/shaders/cutfx.vert.glsl
+++ src/components/shaders/cutfx.vert.glsl
@@ -1,5 +1,4 @@
 varying vec2 uvs;
-varying float cutProgress;
 uniform float progress;
 void main() {
   uvs.xy = uv.xy;
@@ -7,10 +6,7 @@ void main() {
   float isGlow = step(0.5, uvs.y);
   float isArc =  step(0.05, uvs.y) - step(0.51, uvs.y);
   float isCut =  1.0 - step(0.05, uvs.y);
-  cutProgress = clamp(0.0, 1.0, progress * 1.8);
   // move arc sprite
   pos.z -= mix(0.0, -0.5 + progress * 0.6, isArc);
-  // move cut sprite
-  pos.x += mix(0.0, -0.3 + cutProgress * uvs.x * 1.7 , isCut);
   gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
