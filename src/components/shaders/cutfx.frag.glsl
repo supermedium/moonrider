a24bf5d9511b1ca7109d4463d39914127228f36b
Diego F. Goberna <diego@feiss.be>
2019-05-03 14:04:17 +0200

    (fixes #305) simplified cutfx to only colored glow

--- src/components/shaders/cutfx.frag.glsl
+++ src/components/shaders/cutfx.frag.glsl
@@ -4,13 +4,7 @@ uniform vec3 color;
 uniform float progress;
 
 void main() {
-  vec4 tex = texture2D(src, uvs);
-  float isGlow = step(0.5, uvs.y);
-  float isArc =  step(0.05, uvs.y) - step(0.51, uvs.y);
-
-  vec3 col = mix(tex.rgb, color, isGlow);
-
-  float alpha = smoothstep(progress, 1.0, tex.a);
-  alpha = mix(alpha, smoothstep(1.0 - sin(progress * 3.1415), 1.0, tex.a), isArc);
+  vec3 col = color;
+  float alpha = smoothstep(progress, 1.0, texture2D(src, uvs).a);
   gl_FragColor = vec4(col, alpha);
 }
--- src/components/shaders/cutfx.vert.glsl
+++ src/components/shaders/cutfx.vert.glsl
@@ -1,12 +1,5 @@
 varying vec2 uvs;
-uniform float progress;
 void main() {
   uvs.xy = uv.xy;
-  vec3 pos = position;
-  float isGlow = step(0.5, uvs.y);
-  float isArc =  step(0.05, uvs.y) - step(0.51, uvs.y);
-  float isCut =  1.0 - step(0.05, uvs.y);
-  // move arc sprite
-  pos.z -= mix(0.0, -0.5 + progress * 0.6, isArc);
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
