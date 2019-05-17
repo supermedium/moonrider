d097b4519c38e6bf4f302c8be8c86023e253cfe5
Kevin Ngo <me@ngokevin.com>
2019-05-17 13:42:43 -0700

    render order from npm

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ require('aframe-haptics-component');
 require('aframe-layout-component');
 require('aframe-orbit-controls');
 require('aframe-proxy-event-component');
+require('aframe-render-order-component');
 require('aframe-ring-shader');
 require('aframe-state-component');
 require('aframe-slice9-component');
