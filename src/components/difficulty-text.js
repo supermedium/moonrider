7303f902a5fe331cedb05dc5f0a8e08933743775
Kevin Ngo <me@ngokevin.com>
2019-05-17 12:05:41 -0700

    tweak difficulty names (supernova)

---
+++ src/components/difficulty-text.js
@@ -0,0 +1,36 @@
+// Unique difficulty naming.
+AFRAME.registerComponent('difficulty-text', {
+  schema: {
+    difficulty: {value: 'Easy'}
+  },
+
+  update: function () {
+    let text = '';
+    switch (this.data.difficulty) {
+      case 'Easy': {
+        text = 'EASY';
+        break;
+      }
+      case 'Normal': {
+        text = 'NORMAL';
+        break;
+      }
+      case 'Hard': {
+        text = 'HARD';
+        break;
+      }
+      case 'Expert': {
+        text = 'SUPER';
+        break;
+      }
+      case 'Expertplus': {
+        text = 'SUPERNOVA';
+        break;
+      }
+      default: {
+        text = 'SUPERNOVA';
+      }
+    }
+    this.el.setAttribute('text', 'value', text);
+  }
+});
