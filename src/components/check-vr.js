4a1a0a31522fe24817546afb00845ec311affd14
Kevin Ngo <me@ngokevin.com>
2019-05-19 14:40:20 -0700

    hide vr button on mobile

--- src/components/check-vr.js
+++ src/components/check-vr.js
@@ -1,5 +1,9 @@
 AFRAME.registerSystem('check-vr', {
   init: function () {
+    if (AFRAME.utils.device.isMobile()) {
+      document.getElementById('vrButton').style.display = 'none';
+    }
+
     setTimeout(() => {
       if (AFRAME.utils.device.checkHeadsetConnected()) {
         this.el.emit('displayconnected');
