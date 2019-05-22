39f8552c72f103ffdc3978e35d2412b028977257
Kevin Ngo <me@ngokevin.com>
2019-05-21 20:26:22 -0700

    simplify midsection animation toggle system

---
+++ src/components/menu-midsection.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('menu-midsection', {
+  schema: {
+    active: {default: false},
+    selectedChallenge: {default: ''}
+  },
+
+  update: function (oldData) {
+    if (oldData.active && !this.data.active) {
+      this.el.emit('hidedifficultysection', null, false);
+    }
+
+    if ((!oldData.active && this.data.active) ||
+        (this.data.selectedChallenge &&
+         oldData.selectedChallenge !== this.data.selectedChallenge)) {
+      this.el.emit('showdifficultysection', null, false);
+    }
+  }
+});
