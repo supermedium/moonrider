fe2fccc3f091aa9a1c8f80f61e188ceced667273
Kevin Ngo <me@ngokevin.com>
2019-05-30 16:52:04 -0700

    remove unused colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -13,19 +13,6 @@ module.exports = {
   RED2: '#f971c3',
   BLUE2: '#6ff9ea',
 
-  GRAY1: '#4d4b4c',
-  GRAY2: '#343434',
-  GRAY3: '#181818',
-
   UI_ACCENT: '#08bfa2',
-  UI_ACCENT2: '#f01978',
-
-  TEXT_OFF: '#000',
-  TEXT_NORMAL: '#444',
-  TEXT_BOLD: '#888',
-
-  MINE_RED: '#1c060e',
-  MINE_RED_EMISSION: '#2a0d1b',
-  MINE_BLUE: '#011114',
-  MINE_BLUE_EMISSION: '#072020'
+  UI_ACCENT2: '#f01978'
 };
