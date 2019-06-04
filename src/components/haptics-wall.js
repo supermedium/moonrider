c678fb0f658079e4fde64d114f42f69d7cc7526b
Kevin Ngo <me@ngokevin.com>
2019-06-03 20:17:32 -0700

    bump wall haptics

--- src/components/haptics-wall.js
+++ src/components/haptics-wall.js
@@ -2,10 +2,14 @@
  * Listen to aabb-collider event for wall haptics.
  */
 AFRAME.registerComponent('haptics-wall', {
+  schema: {
+    enabled: {default: false}
+  },
+
   init: function () {
     const el = this.el;
     this.isHittingWall = false;
-    el.setAttribute('haptics__wall', {dur: 50, force: 0.075});
+    el.setAttribute('haptics__wall', {dur: 50, force: 0.15});
 
     this.checkIfHittingWall = this.checkIfHittingWall.bind(this);
     el.addEventListener('mouseenter', this.checkIfHittingWall);
@@ -29,7 +33,7 @@ AFRAME.registerComponent('haptics-wall', {
   },
 
   tick: function () {
-    if (!this.isHittingWall) { return; }
+    if (!this.data.enabled || !this.isHittingWall) { return; }
     this.el.components.haptics__wall.pulse();
   }
 });
