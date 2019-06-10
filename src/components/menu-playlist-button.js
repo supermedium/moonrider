6dc7278e5e4da77c74ea130a54ee71c1ec867e88
Kevin Ngo <me@ngokevin.com>
2019-06-09 20:44:17 -0700

    fix playlist pulse anim

--- src/components/menu-playlist-button.js
+++ src/components/menu-playlist-button.js
@@ -13,27 +13,30 @@ AFRAME.registerComponent('menu-playlist-button', {
     el.setAttribute('animation__scaleup', {
       property: 'scale',
       from: '1 1 1',
-      to: '1.1 1.1 1.1',
-      dur: 600,
+      to: '1.2 1.2 1.2',
+      dur: 800,
       autoplay: false,
       easing: 'easeInOutElastic'
     });
 
     el.setAttribute('animation__scaledown', {
       property: 'scale',
-      from: '1.1 1.1 1.1',
+      from: '1.2 1.2 1.2',
       to: '1 1 1',
       dur: 100,
-      startEvents: 'animationcomplete__scaleup',
+      autoplay: false,
       easing: 'easeInOutCubic'
     });
   },
 
   update: function (oldData) {
     // Play animation when song is favorited.
-    if (!oldData.isFavorited && this.data.isFavorited &
+    if (!oldData.isFavorited && this.data.isFavorited &&
         oldData.selectedChallenge === this.data.selectedChallenge) {
       this.el.components['animation__scaleup'].beginAnimation();
+      setTimeout(() => {
+        this.el.components['animation__scaledown'].beginAnimation();
+      }, 800);
     }
   }
 });
