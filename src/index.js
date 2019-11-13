b9b0a9ef71dece27e33d365216d0b35b0c325003
Kevin Ngo <me@ngokevin.com>
2019-11-12 16:55:30 -0800

    newsletter form

--- src/index.js
+++ src/index.js
@@ -33,3 +33,68 @@ require('./index.css');
 require('./scene.html');
 
 if (module.hot) { module.hot.accept(); }
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSubscribeForm();
+});
+
+/**
+ * Init XHR handler to subscribe.
+ */
+function initSubscribeForm () {
+  const form = document.querySelector('form');
+  if (!form) { return; }
+
+  const button = form.querySelector('.submit');
+  const input = form.querySelector('input[type="email"]');
+  const newsletterHeader = document.querySelector('#subscribeForm > h2');
+
+  let originalHeaderText = '';
+  if (newsletterHeader) {
+    originalHeaderText = newsletterHeader.innerHTML;
+  }
+
+  form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    // supermedium/superchimp
+    const xhr = new XMLHttpRequest();
+    let endpoint = 'http://localhost:5000/mail/subscribe';
+    if (process.env.NODE_ENV === 'production') {
+      endpoint = 'https://supermedium.com/mail/subscribe';
+    }
+    xhr.open('POST', endpoint);
+
+    xhr.addEventListener('load', () => {
+      if (parseInt(xhr.status, 10) !== 200) {
+        window.location.href = 'https://supermedium/subscribe/';
+      }
+      if (button) {
+        button.disabled = true;
+        button.innerHTML = 'Subscribed!';
+      }
+      if (newsletterHeader) {
+        newsletterHeader.innerHTML = 'Successfully subscribed, thank you!';
+      }
+    });
+
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.send(JSON.stringify({
+      email: document.querySelector('[name="email"]').value
+    }));
+
+    return false;
+  });
+
+  if (button) {
+    input.addEventListener('keydown', () => {
+      if (button.hasAttribute('disabled')) {
+        button.value = 'Subscribe';
+        button.removeAttribute('disabled');
+      }
+      if (newsletterHeader && originalHeaderText) {
+        newsletterHeader.innerHTML = originalHeaderText;
+      }
+    });
+  }
+}
