20c5ef00bbd0d4450dd44e00ab81a9214c52dda6
Kevin Ngo <me@ngokevin.com>
2020-02-25 13:58:51 -0800

    remove song preview super preloading system to reduce bandwidth

--- src/components/song-preview.js
+++ src/components/song-preview.js
@@ -3,277 +3,28 @@ var utils = require('../utils');
 const PREVIEW_VOLUME = 0.5;
 
 /**
- * Song preview when search result selected with smart logic for preloading.
+ * Song previews.
  */
 AFRAME.registerComponent('song-preview-system', {
   schema: {
-    challengeId: {default: ''},
-    debug: {default: false},
-    isSearching: {default: false},
-    isLoading: {default: false},  // Continue to play preview song during loading.
+    previewStartTime: {type: 'int'},
     selectedChallengeId: {type: 'string'}
   },
 
   init: function () {
-    this.analyserEl = document.getElementById('audioAnalyser');
-    this.audio = document.createElement('audio');  // Dummy.
-    this.audioStore = {};
-    this.preloadedAudioIds = [];
-    this.preloadQueue = [];
-  },
-
-  update: function (oldData) {
-    const data = this.data;
-
-    // Play clicked.
-    // But don't start playing it if it wasn't playing already.
-    if (!oldData.challengeId && data.challengeId) {
-      if (this.analyserEl.components.audioanalyser.volume === 0 ||
-          this.audio.currentTime < 1) {
-        this.stopSong();
-      }
-      // this.fadeOut();
-      return;
-    }
-
-    // Unselect.
-    if (oldData.selectedChallengeId && !data.selectedChallengeId && !data.isSearching) {
-      this.stopSong();
-    }
-
-    // Song finished loading.
-    // Continue to play preview song during loading to keep entertained.
-    if (oldData.isLoading && !data.isLoading) {
-      this.stopSong();
-      return;
-    }
-
-    // Selected challenge ID updated.
-    if (data.selectedChallengeId && oldData.selectedChallengeId !== data.selectedChallengeId) {
-      if (this.audio) { this.stopSong(); }
-
-      // If not yet preloaded, pause the preload queue until this song is loaded.
-      if (!this.preloadedAudioIds.includes(data.selectedChallengeId) &&
-          data.selectedChallengeId !== this.currentLoadingId) {
-        this.prioritizePreloadSong();
-      }
-
-      this.playSong(data.selectedChallengeId);
-    }
-  },
-
-  log: function (str) {
-    if (!this.data.debug) { return; }
-    console.log(str);
-  },
-
-  /**
-   * Song was selected so pause preload queue, prioritize its loading, and try to play ASAP.
-   */
-  prioritizePreloadSong: function () {
-    const data = this.data;
-    const preloadQueue = this.preloadQueue;
-
-    if (!this.audioStore[data.selectedChallengeId]) {
-      document.querySelector(`[song-preview][data-bind-for-key="${data.selectedChallengeId}"]`).components['song-preview'].queue();
-    }
-
-    this.log(`[song-preview] Prioritizing loading of ${data.selectedChallengeId}`);
-    this.priorityLoadingChallengeId = data.selectedChallengeId;
-
-    this.audioStore[data.selectedChallengeId].addEventListener('loadeddata', () => {
-      this.log(`[song-preview] Finished load of priority ${data.selectedChallengeId}`);
-      if (this.preloadedAudioIds.indexOf(data.selectedChallengeId) === -1) {
-        this.preloadedAudioIds.push(data.selectedChallengeId);
-      }
-      this.truncateCache();
-
-      this.priorityLoadingChallengeId = '';
-      // Resume preloading queue.
-      if (preloadQueue.length) {
-        this.log(`[song-preview] Resuming queue with ${preloadQueue[0].challengeId}`);
-        this.preloadMetadata(preloadQueue[0]);
-      }
-    });
-
-    // Preload.
-    this.audioStore[data.selectedChallengeId].src =
-      utils.getS3FileUrl(data.selectedChallengeId, 'song.ogg');
-
-    // Remove from preload queue.
-    for (let i = 0; i < preloadQueue.length; i++) {
-      if (preloadQueue[i].challengeId === data.selectedChallengeId) {
-        preloadQueue.splice(i, 1);
-        break;
-      }
-    }
-  },
-
-  /**
-   * Create an audio element and queue to preload. If the queue is empty, preload it
-   * immediately.
-   */
-  queuePreloadSong: function (challengeId, previewStartTime) {
-    if (this.audioStore[challengeId]) { return; }
-
-    const audio = document.createElement('audio');
-    audio.crossOrigin = 'anonymous';
-    audio.dataset.previewStartTime = previewStartTime;
-    this.audioStore[challengeId] = audio;
-
-    let src = utils.getS3FileUrl(challengeId, 'song.ogg');
-    if (this.currentLoadingId) {
-      // Audio currently loading, add to queue.
-      this.preloadQueue.push({
-        audio: audio,
-        challengeId: challengeId,
-        src: src
-      });
-    } else {
-      // Empty queue, preload now.
-      this.preloadMetadata({
-        audio: audio,
-        challengeId: challengeId,
-        src: src
-      });
-    }
-  },
-
-  /**
-   * Preload metadata of audio file for quick play.
-   * Set `src` and `preload`.
-   * A preload queue is set up so we only preload one at a time to not bog down
-   * the network. If a song is selected to preview, we can bump it to the front of the
-   * queue.
-   */
-  preloadMetadata: function (preloadItem) {
-    const audio = preloadItem.audio;
-    this.log(`[song-preview] Preloading song preview ${preloadItem.challengeId} ${preloadItem.src}`);
-
-    audio.addEventListener('loadedmetadata', () => {
-      // Song preloaded.
-      this.log(`[song-preview] Finished preloading song preview ${preloadItem.challengeId}`);
-      if (this.preloadedAudioIds.indexOf(preloadItem.challengeId) === -1) {
-        this.preloadedAudioIds.push(preloadItem.challengeId);
-      }
-      this.truncateCache();
-
-      this.currentLoadingId = '';
-
-      // Move on to next song in queue if any.
-      this.log(`[song-preview] ${this.preloadQueue.length} in queue`);
-      if (this.preloadQueue.length && !this.priorityLoadingChallengeId) {
-        this.preloadMetadata(this.preloadQueue.shift());
-      }
-    });
-
-    audio.addEventListener('error', () => {
-      this.log(`[song-preview] Error preloading song preview ${preloadItem.challengeId}`);
-
-      this.currentLoadingId = '';
-
-      // Move on to next song in queue if any.
-      this.log(`[song-preview] ${this.preloadQueue.length} in queue`);
-      if (this.preloadQueue.length && !this.priorityLoadingChallengeId) {
-        this.preloadMetadata(this.preloadQueue.shift());
-      }
-    });
-
-    audio.preload = 'metadata';
-    audio.src = preloadItem.src;
-    this.currentLoadingId = preloadItem.challengeId;
-  },
-
-  stopSong: function () {
-    if (!this.audio) { return; }
-    if (!this.audio.paused) { this.audio.pause(); }
-  },
-
-  playSong: function (challengeId) {
-    if (!challengeId) { return; }
-
-    this.audio = this.audioStore[challengeId];
-    this.audio.load();
+    this.audio = document.createElement('audio');
     this.audio.volume = PREVIEW_VOLUME;
-    this.audio.currentTime = this.audio.dataset.previewStartTime;
-    this.analyserEl.setAttribute('audioanalyser', 'src', this.audio);
-
-    const audioanalyser = this.analyserEl.components.audioanalyser;
-    audioanalyser.resumeContext();
-
-    this.audio.play();
-    // this.fadeIn();
-
-    // Prefetch buffer for playing.
-    if (audioanalyser.xhr) { audioanalyser.xhr.abort(); }
-    audioanalyser.fetchAudioBuffer(utils.getS3FileUrl(challengeId, 'song.ogg'));
-  },
-
-  /**
-   * Stop song from preloading.
-   */
-  clearSong: function (challengeId) {
-    let audio = this.audioStore[challengeId];
-
-    if (audio) {
-      audio.src = '';
-      audio.preload = 'none';
-      delete this.audioStore[challengeId];
-    }
-
-    // Remove from queue if in there.
-    let index;
-    for (let i = 0; i < this.preloadQueue.length; i++) {
-      if (this.preloadQueue[i].id === challengeId) {
-        index = i;
-        break;
-      }
-    }
-    if (!index) { return; }
-    this.preloadQueue.splice(index, 1);
-  },
-
-  /**
-   * Only keep the most recent previews to keep memory low.
-   */
-  truncateCache: function () {
-    const ids = this.preloadedAudioIds;
-    while (this.preloadedAudioIds.length > 12) {
-      const id = this.preloadedAudioIds.shift()
-      if (!this.audioStore[id]) { continue; }
-      if (id === this.data.selectedChallengeId) { continue; }
-      if (this.audioStore[id].paused) {
-        this.clearSong(id);
-        delete this.audioStore[id];
-        this.log(`[song-preview-system] Clearing ${id}`);
-      }
-    }
-    while (this.preloadQueue.length > 12) {
-      const id = this.preloadQueue.shift()
-      if (this.currentLoadingId === id) { this.currentLoadingId = null; }
-    }
-  }
-});
-
-/**
- * Data component attached to search result for song preview system.
- */
-AFRAME.registerComponent('song-preview', {
-  dependencies: ['song-preview-system'],
-
-  schema: {
-    challengeId: {type: 'string'},
-    previewStartTime: {type: 'number'}
   },
 
   update: function (oldData) {
-    if (!this.el.sceneEl.components['song-preview-system']) { return; }
-    this.queue();
-  },
+    const data = this.data;
 
-  queue: function () {
-    this.el.sceneEl.components['song-preview-system'].queuePreloadSong(
-      this.data.challengeId, this.data.previewStartTime
-    );
-  }
+    this.audio.pause();
+
+    if (data.selectedChallengeId && oldData.selectedChallengeId !== data.selectedChallengeId) {
+      this.audio.setAttribute('src', utils.getS3FileUrl(data.selectedChallengeId, 'song.ogg'));
+      this.audio.currentTime = data.previewStartTime;
+      this.audio.play();
+    }
+  },
 });
