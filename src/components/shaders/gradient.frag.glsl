be5872c33fbddd4d1934138621a1ae77a6ca7ed5
Kevin Ngo <ktngo09@gmail.com>
2020-04-30 14:40:37 -0700

    fix color gradients on quest

--- src/components/shaders/gradient.frag.glsl
+++ src/components/shaders/gradient.frag.glsl
@@ -5,5 +5,5 @@ uniform vec3 color2;
 void main () {
   float mixDistance = distance(uvs, vec2(0.5, 0));
   float alphaDistance = 0.5 - distance(uvs, vec2(0.5));
-  gl_FragColor = vec4(mix(color1, color2, pow(mixDistance, 2.4)), sqrt(alphaDistance));
+  gl_FragColor = vec4(mix(color1, color2, pow(mixDistance, 2.4)), alphaDistance);
 }
